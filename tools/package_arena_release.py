8acdd517291460e1056ca54d2ed39cca62a03098
tenP0312-dev <behkuq54@gmail.com>
2026-08-12 03:03:44 +0900

    Prepare Arena oraja 0.4.14.35 (#42)

--- tools/package_arena_release.py
+++ tools/package_arena_release.py
@@ -22,7 +22,7 @@ import tempfile
 import zipfile
 
 
-VERSION = "0.4.14.34"
+VERSION = "0.4.14.35"
 BODY_FILENAME = "Arena-oraja.jar"
 PLUGIN_FILENAME = "bms_ir_arena_oraja_0.0.69.jar"
 PLATFORM_SPECS = {
