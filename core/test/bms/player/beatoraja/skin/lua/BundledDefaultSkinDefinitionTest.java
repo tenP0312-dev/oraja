c4139a13c023cebbb41962adeb129b2bb5a11a54
tenP0312-dev <behkuq54@gmail.com>
2026-08-19 10:54:24 +0900

    Redesign default Music Select for FHD (#225)

--- core/test/bms/player/beatoraja/skin/lua/BundledDefaultSkinDefinitionTest.java
+++ core/test/bms/player/beatoraja/skin/lua/BundledDefaultSkinDefinitionTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 
@@ -24,6 +25,8 @@ class BundledDefaultSkinDefinitionTest {
 		assertNotNull(loader.loadHeader(path));
 
 		JsonSkin.Skin skin = loader.parsedSkin();
+		assertEquals(1920, skin.w);
+		assertEquals(1080, skin.h);
 		assertEquals(9, imageSet(skin, "sortset").images.length);
 		assertEquals(4, imageSet(skin, "bmsir-extra").images.length);
 		assertTrue(hasDestination(skin, "bmsir-restore-off"));
@@ -31,23 +34,57 @@ class BundledDefaultSkinDefinitionTest {
 		assertTrue(hasValue(skin, "ir-rank"));
 		assertTrue(hasValue(skin, "folder-fullcombo"));
 		assertTrue(hasDestination(skin, "comment-panel"));
+		assertTrue(hasDestination(skin, "popbelt-ui-base"));
+		assertTrue(hasDestination(skin, "popbelt-ui-top"));
 		assertFalse(hasDestination(skin, "notes-graph"));
 		assertFalse(hasDestination(skin, "bpmgraph"));
 
 		JsonSkin.Text tableComment = text(skin, "tablecomment");
 		assertTrue(tableComment.wrapping);
-		assertEquals(12, tableComment.size);
+		assertEquals(18, tableComment.size);
 		JsonSkin.Destination tableCommentDestination = destination(skin, "tablecomment");
-		assertEquals(90, tableCommentDestination.dst[0].x);
-		assertEquals(274, tableCommentDestination.dst[0].y);
-		assertEquals(580, tableCommentDestination.dst[0].w);
-		assertEquals(14, tableCommentDestination.dst[0].h);
+		assertEquals(120, tableCommentDestination.dst[0].x);
+		assertEquals(286, tableCommentDestination.dst[0].y);
+		assertEquals(1000, tableCommentDestination.dst[0].w);
+		assertEquals(28, tableCommentDestination.dst[0].h);
 		assertEquals(1, skin.radargraph.length);
+		assertEquals(652, destination(skin, "radar1").dst[0].y);
+		assertEquals(630, destination(skin, "playlevel").dst[0].y);
+		assertEquals(505, destination(skin, "score").dst[0].y);
 
 		var placeholder = ImageIO.read(Path.of("../assets/skin/default/bmsir-controls-placeholder.png").toFile());
 		assertNotNull(placeholder);
 		assertEquals(224, placeholder.getWidth());
 		assertEquals(32, placeholder.getHeight());
+
+		for (String name : new String[] {"select.png", "select2.png", "select3.png"}) {
+			var background = ImageIO.read(Path.of("../assets/skin/default/select/background", name).toFile());
+			assertNotNull(background);
+			assertEquals(1920, background.getWidth());
+			assertEquals(1080, background.getHeight());
+		}
+
+		var baseUi = ImageIO.read(Path.of("../assets/skin/default/select/popbelt-ui-base.png").toFile());
+		assertNotNull(baseUi);
+		assertEquals(1920, baseUi.getWidth());
+		assertEquals(1080, baseUi.getHeight());
+
+		var topUi = ImageIO.read(Path.of("../assets/skin/default/select/popbelt-ui-top.png").toFile());
+		assertNotNull(topUi);
+		assertEquals(1920, topUi.getWidth());
+		assertEquals(1080, topUi.getHeight());
+
+		var songBar = ImageIO.read(Path.of("../assets/skin/default/songbar.png").toFile());
+		assertNotNull(songBar);
+		assertEquals(750, songBar.getWidth());
+		assertEquals(432, songBar.getHeight());
+
+		assertTrue(Files.isRegularFile(Path.of("../assets/skin/default/select/background/select.svg")));
+		assertTrue(Files.isRegularFile(Path.of("../assets/skin/default/select/background/select2.svg")));
+		assertTrue(Files.isRegularFile(Path.of("../assets/skin/default/select/background/select3.svg")));
+		assertTrue(Files.isRegularFile(Path.of("../assets/skin/default/select/popbelt-ui-base.svg")));
+		assertTrue(Files.isRegularFile(Path.of("../assets/skin/default/select/popbelt-ui-top.svg")));
+		assertTrue(Files.isRegularFile(Path.of("../assets/skin/default/songbar.svg")));
 	}
 
 	@Test
